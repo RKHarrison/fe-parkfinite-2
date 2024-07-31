c8e6368593cff3b18e593755108949479b6313a3
Richard Harrison <r.harr.k@gmail.com>
2024-07-31 13:36:02 +0100

    renders map in mac screen, using google api as provider, and setting uk as initial region

--- parkfinite2/app/(drawer)/(tabs)/search/map.tsx
+++ parkfinite2/app/(drawer)/(tabs)/search/map.tsx
@@ -1,37 +1,37 @@
-import { Pressable, Text, View } from "react-native";
-import { Link, router } from "expo-router";
 import React from "react";
-import { ThemedText } from "@/components/ThemedText";
+import MapView from "react-native-maps";
+import { PROVIDER_GOOGLE } from "react-native-maps";
 
-export default function Map() {
-  let campsiteId = 2;
+import { StyleSheet, View } from "react-native";
 
+const INITIAL_REGION = {
+  latitude: 53.00,
+  longitude: -4.40,
+  latitudeDelta: 11.0,
+  longitudeDelta: 11.0,
+}
+
+
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit (drawer)/(tabs)/search/map.tsx to edit this screen.</Text>
-      <Link href="/(drawer)/(tabs)/search/campsites/1">
-        <ThemedText type="link">
-          View full campsite details for campsite 1...
-        </ThemedText>
-      </Link>
-      <Pressable
-        onPress={() =>
-          router.push(`/(drawer)/(tabs)/search/campsites/${campsiteId}`)
-        }
-      >
-        <ThemedText type="link">
-          View full campsite details for campsite {campsiteId}...
-        </ThemedText>
-      </Pressable>
-      <Link href="/(drawer)/(tabs)/new-campsite">
-        <ThemedText type="link">Post a new camping location...</ThemedText>
-      </Link>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={INITIAL_REGION}
+        showsUserLocation
+        showsMyLocationButton
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
