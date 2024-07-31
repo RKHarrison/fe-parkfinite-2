626a6f0b891e92862d6be897eb6a63bd59c3f6b2
Richard Harrison <r.harr.k@gmail.com>
2024-07-31 16:59:26 +0100

    creates api request to get all campsites

---
+++ parkfinite2/services/api/campsitesApi.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const parkfinite2Api = axios.create({
+  baseURL: "https://parkfinite-api.onrender.com",
+});
+
+export const getCampsites = () => {
+  return parkfinite2Api
+    .get<Campsite[]>("/campsites")
+    .then((res) => {
+      return res.data;
+    })
+    .catch((error) => {
+      console.error("API Error", {
+        message: error.message,
+        status: error.response?.status,
+        data: error.response?.data,
+      });
+      throw error;
+    });
+};
