67cc6b4778d2dbe47b032d0331258ffca8392395
Richard Harrison <r.harr.k@gmail.com>
2024-07-31 17:03:00 +0100

    adds functionality to map component to request campsites from api and render them with custom marker icon

--- parkfinite2/app/(drawer)/(tabs)/search/map.tsx
+++ parkfinite2/app/(drawer)/(tabs)/search/map.tsx
@@ -1,24 +1,31 @@
 import React from "react";
-import { useEffect } from 'react';
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, Image } from "react-native";
 import MapView from "react-native-maps";
-import { PROVIDER_GOOGLE } from "react-native-maps";
+import { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
+import { getCampsites } from "@/services/api/campsitesApi";
+import { Campsite } from "@/types/campsite";
+import campsiteIcon from "@/assets/images/camping-location-icon.png";
 
 const INITIAL_REGION = {
-  latitude: 53.00,
-  longitude: -4.40,
+  latitude: 53.0,
+  longitude: -4.4,
   latitudeDelta: 11.0,
   longitudeDelta: 11.0,
-}
-
+};
 
 export default function Map() {
+  const [loadedCampsites, setLoadedCampsites] = useState<Campsite[]>([]);
 
   useEffect(() => {
     Location.requestForegroundPermissionsAsync();
+    getCampsites()
+      .then((campsitesFromApi) => {
+        return setLoadedCampsites(campsitesFromApi);
+      })
+      .catch((err) => console.error("Failed to load campsites", err));
   }, []);
-
 
   return (
     <View style={styles.container}>
@@ -31,7 +38,23 @@ export default function Map() {
         showsUserLocation={true}
         showsMyLocationButton={true}
         showsTraffic={false}
-      />
+      >
+        {loadedCampsites.map((campsite, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: campsite.campsite_latitude,
+              longitude: campsite.campsite_longitude,
+            }}
+          >
+            <Image
+              source={campsiteIcon}
+              style={{ width: 30, height: 30 }}
+              resizeMode="contain"
+            />
+          </Marker>
+        ))}
+      </MapView>
     </View>
   );
 }
