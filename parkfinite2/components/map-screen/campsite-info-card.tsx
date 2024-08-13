9e404f40aa36e60f3ec535752a7d48322bac20c0
Richard Harrison <r.harr.k@gmail.com>
2024-08-13 14:19:17 +0100

    adds a picture to campsite-info-card if avaiable, and formats to flex existing test around it

--- parkfinite2/components/map-screen/campsite-info-card.tsx
+++ parkfinite2/components/map-screen/campsite-info-card.tsx
@@ -1,35 +1,63 @@
 import { Campsite } from "@/types/api-data-types/campsite-types";
 import { Collapsible } from "../Collapsible";
-import { Text } from "react-native";
+import { Text, Image, View } from "react-native";
 import { convertNumberToStars } from "@/utils/convertNumberToStars";
 import { StyleSheet, ViewStyle } from "react-native";
+import { useEffect } from "react";
 
 export function CampsiteInfoCard({
   selectedCampsite,
 }: {
   selectedCampsite: Campsite;
 }) {
+
+
   return (
-    <Collapsible title={"Campsite Info"} collapsibleContainerStyle={styles.collapsibleContainer}>
+<Collapsible title="Campsite Info" collapsibleContainerStyle={styles.collapsibleContainer}>
+  <View style={styles.contentContainer}>
+    {selectedCampsite?.photos[0]?.campsite_photo_url && (
+      <Image
+        source={{ uri: selectedCampsite.photos[0].campsite_photo_url }}
+        style={styles.image}
+      />
+    )}
+    <View style={styles.textContainer}>
       <Text style={styles.header}>{selectedCampsite?.campsite_name}</Text>
-      <Text>{convertNumberToStars(selectedCampsite?.average_rating)}  </Text>
+      <Text>{convertNumberToStars(selectedCampsite?.average_rating)}</Text>
       <Text>{selectedCampsite?.description}</Text>
-    </Collapsible>
+    </View>
+  </View>
+</Collapsible>
   );
 }
 
 const styles = StyleSheet.create({
-    header: {
-        fontWeight: "bold",
-    },
     collapsibleContainer: {
-        width: "76%",        
-        zIndex: 1000,
-        left: 50,
-        right: 50,
-        bottom: 70,
-        borderRadius: 5,
-        paddingRight: 30,
-        padding: 8,
-    }
-})
+      width: "76%",
+      zIndex: 1000,
+      left: 50,
+      right: 50,
+      bottom: 70,
+      borderRadius: 5,
+      paddingRight: 30,
+      padding: 8,
+    },
+    contentContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+    },
+    image: {
+      width: 100,
+      height: 100,
+      borderRadius: 10,
+      marginRight: 10,
+    },
+    textContainer: {
+      flex: 1,
+      justifyContent: "center",
+    },
+    header: {
+      fontWeight: "bold",
+      marginBottom: 4,
+    },
+  });
