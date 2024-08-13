46387d4e5f2e13cde7e113255857f73691aa2257
Richard Harrison <r.harr.k@gmail.com>
2024-08-13 11:14:40 +0100

    renders campsite info component when a campsite is selected

--- parkfinite2/components/map-screen/map-component.tsx
+++ parkfinite2/components/map-screen/map-component.tsx
@@ -1,22 +1,25 @@
-import React, { useEffect, useState } from "react";
-import MapView from "react-native-map-clustering";
-import { PROVIDER_GOOGLE, Marker } from "react-native-maps";
-import { StyleSheet, Image } from "react-native";
-import * as Location from "expo-location";
 import { getCampsites } from "@/services/api/campsitesApi";
 import { Campsite } from "@/types/api-data-types/campsite-types";
 import { Region } from "@/types/locations";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
+import { Image, StyleSheet, View, Text } from "react-native";
+import MapView from "react-native-map-clustering";
+import { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
-import icon1 from "@/assets/images/campsite-icons/in-nature-icon.png";
-import icon2 from "@/assets/images/campsite-icons/carpark-icon.png";
+import { Collapsible } from "../Collapsible";
+import { CampsiteInfoCard } from "./campsite-info-card";
+
+import icon10 from "@/assets/images/campsite-icons/beach-icon.png";
+import icon8 from "@/assets/images/campsite-icons/camping-icon.png";
 import icon3 from "@/assets/images/campsite-icons/carpark-day-only-icon.png";
-import icon4 from "@/assets/images/campsite-icons/motorway-icon.png";
+import icon2 from "@/assets/images/campsite-icons/carpark-icon.png";
+import icon1 from "@/assets/images/campsite-icons/in-nature-icon.png";
 import icon5 from "@/assets/images/campsite-icons/motorhome-free-icon.png";
 import icon6 from "@/assets/images/campsite-icons/motorhome-paid-icon.png";
 import icon7 from "@/assets/images/campsite-icons/motorhome-private-icon.png";
-import icon8 from "@/assets/images/campsite-icons/camping-icon.png";
+import icon4 from "@/assets/images/campsite-icons/motorway-icon.png";
 import icon9 from "@/assets/images/campsite-icons/picnic-icon.png";
-import icon10 from "@/assets/images/campsite-icons/beach-icon.png";
 
 type IconKey = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
@@ -46,46 +49,47 @@ export default function MapComponent({ region }: { region: Region }) {
       .catch((err) => console.error("Failed to load campsites", err));
   }, []);
 
-
-  useEffect(()=> {
+  useEffect(() => {
     console.log(selectedCampsite?.campsite_name);
-    
-  },[selectedCampsite])
+  }, [selectedCampsite]);
 
   return (
-    <MapView
-      style={styles.map}
-      provider={PROVIDER_GOOGLE}
-      loadingEnabled={true}
-      initialRegion={region}
-      region={region}
-      showsUserLocation={true}
-      showsMyLocationButton={true}
-      followsUserLocation={true}
-      showsTraffic={false}
-      // cluster controls
-      clusterColor="#88c9ffBF"
-      minPoints={6}
-    >
-      {loadedCampsites.map((campsite, index) => (
-        <Marker
-          key={campsite.campsite_id}
-          onPress={() => setSelectedCampsite(campsite)}
-          coordinate={{
-            latitude: campsite.campsite_latitude,
-            longitude: campsite.campsite_longitude,
-          }}
-          title={campsite.campsite_name}
-          description={campsite.category.category_name}
-        >
-          <Image
-            source={icons[campsite.category.category_id as IconKey]}
-            style={{ width: 30, height: 30 }}
-            resizeMode="contain"
-          />
-        </Marker>
-      ))}
-    </MapView>
+    <>
+      {selectedCampsite && <CampsiteInfoCard selectedCampsite={selectedCampsite} />}
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        loadingEnabled={true}
+        initialRegion={region}
+        region={region}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        followsUserLocation={true}
+        showsTraffic={false}
+        // cluster controls
+        clusterColor="#88c9ffBF"
+        minPoints={6}
+      >
+        {loadedCampsites.map((campsite, index) => (
+          <Marker
+            key={campsite.campsite_id}
+            onPress={() => setSelectedCampsite(campsite)}
+            coordinate={{
+              latitude: campsite.campsite_latitude,
+              longitude: campsite.campsite_longitude,
+            }}
+            title={campsite.campsite_name}
+            description={campsite.category.category_name}
+          >
+            <Image
+              source={icons[campsite.category.category_id as IconKey]}
+              style={{ width: 30, height: 30 }}
+              resizeMode="contain"
+            />
+          </Marker>
+        ))}
+      </MapView>
+    </>
   );
 }
 
