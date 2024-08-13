e8b9ae1de0b1cd770730b5bea829838971fa096f
Richard Harrison <r.harr.k@gmail.com>
2024-08-13 16:48:57 +0100

    creates stylesheet

--- parkfinite2/components/individual-campsite-screen/individual-campsite-card-component.tsx
+++ parkfinite2/components/individual-campsite-screen/individual-campsite-card-component.tsx
@@ -1,7 +1,7 @@
 import { getCampsiteById } from "@/services/api/campsitesApi";
 import { Campsite } from "@/types/api-data-types/campsite-types";
 import { useEffect, useState } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export default function IndividualCampsiteCard({
   id,
@@ -21,22 +21,34 @@ export default function IndividualCampsiteCard({
   }, [loadedCampsite]);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>{loadedCampsite?.campsite_name}</Text>
-      <Text>{loadedCampsite?.category.category_name}</Text>
-      <Text>Average user rating: {loadedCampsite?.average_rating}</Text>
-      <Text>Added on {loadedCampsite?.date_added} by {loadedCampsite?.added_by}</Text>
-      <Text>Description: {loadedCampsite?.description}</Text>
-      <Text>CONTACT INFO</Text>
-      <Text>Name: {loadedCampsite?.contacts[0].campsite_contact_name}</Text>
-      <Text>Phone: {loadedCampsite?.contacts[0].campsite_contact_phone}</Text>
-      <Text>Email: {loadedCampsite?.contacts[0].campsite_contact_email}</Text>
-    </View>
+    <>
+      <View
+        style={styles.container}
+      >
+        <Text>{loadedCampsite?.campsite_name}</Text>
+        <Text>{loadedCampsite?.category.category_name}</Text>
+        <Text>Average user rating: {loadedCampsite?.average_rating}</Text>
+        <Text>
+          Added on {loadedCampsite?.date_added} by {loadedCampsite?.added_by}
+        </Text>
+        <Text>Description: {loadedCampsite?.description}</Text>
+      </View>
+      <View
+        style={styles.container}
+      >
+        <Text>CONTACT INFO</Text>
+        <Text>Name: {loadedCampsite?.contacts[0].campsite_contact_name}</Text>
+        <Text>Phone: {loadedCampsite?.contacts[0].campsite_contact_phone}</Text>
+        <Text>Email: {loadedCampsite?.contacts[0].campsite_contact_email}</Text>
+      </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
