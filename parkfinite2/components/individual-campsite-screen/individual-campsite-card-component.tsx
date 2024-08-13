96aed278840e9e667ee548724ab8aa5182337507
Richard Harrison <r.harr.k@gmail.com>
2024-08-13 16:43:23 +0100

    adds basic text info about selected campsite

--- parkfinite2/components/individual-campsite-screen/individual-campsite-card-component.tsx
+++ parkfinite2/components/individual-campsite-screen/individual-campsite-card-component.tsx
@@ -1,7 +1,7 @@
 import { getCampsiteById } from "@/services/api/campsitesApi";
 import { Campsite } from "@/types/api-data-types/campsite-types";
 import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 export default function IndividualCampsiteCard({
   id,
@@ -28,7 +28,15 @@ export default function IndividualCampsiteCard({
         alignItems: "center",
       }}
     >
-      <Text>Details for campsite {id}</Text>
+      <Text>{loadedCampsite?.campsite_name}</Text>
+      <Text>{loadedCampsite?.category.category_name}</Text>
+      <Text>Average user rating: {loadedCampsite?.average_rating}</Text>
+      <Text>Added on {loadedCampsite?.date_added} by {loadedCampsite?.added_by}</Text>
+      <Text>Description: {loadedCampsite?.description}</Text>
+      <Text>CONTACT INFO</Text>
+      <Text>Name: {loadedCampsite?.contacts[0].campsite_contact_name}</Text>
+      <Text>Phone: {loadedCampsite?.contacts[0].campsite_contact_phone}</Text>
+      <Text>Email: {loadedCampsite?.contacts[0].campsite_contact_email}</Text>
     </View>
   );
 }
