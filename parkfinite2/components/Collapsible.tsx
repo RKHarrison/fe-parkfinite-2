4c5d0d6ee2690e7375e4bd909c5b4bbe6883be91
Richard Harrison <r.harr.k@gmail.com>
2024-08-13 13:46:39 +0100

    creates ability to style collapsible container via props

--- parkfinite2/components/Collapsible.tsx
+++ parkfinite2/components/Collapsible.tsx
@@ -1,28 +1,37 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { PropsWithChildren, useState } from 'react';
-import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { PropsWithChildren, useState } from "react";
+import { StyleSheet, TouchableOpacity, useColorScheme, ViewStyle } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { Colors } from '@/constants/Colors';
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Colors } from "@/constants/Colors";
 
-export function Collapsible({ children, title }: PropsWithChildren & { title: string }) {
+export type CollapsibleContainerStyle = ViewStyle;
+
+export function Collapsible({
+  children,
+  title,
+  collapsibleContainerStyle
+}: PropsWithChildren & { title: string, collapsibleContainerStyle?: CollapsibleContainerStyle }) {
   const [isOpen, setIsOpen] = useState(false);
-  const theme = useColorScheme() ?? 'light';
+  const theme = useColorScheme() ?? "light";
 
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView style={[styles.container, collapsibleContainerStyle]}>
       {isOpen && <ThemedView style={styles.content}>{children}</ThemedView>}
       <TouchableOpacity
         style={styles.heading}
         onPress={() => setIsOpen((value) => !value)}
-        activeOpacity={0.8}>
+        activeOpacity={0.8}
+      >
         <Ionicons
-          name={isOpen ? 'chevron-up' : 'chevron-forward-outline'}
+          name={isOpen ? "chevron-up" : "chevron-forward-outline"}
           size={18}
-          color={theme === 'light' ? Colors.light.icon : Colors.dark.icon}
+          color={theme === "light" ? Colors.light.icon : Colors.dark.icon}
         />
-        <ThemedText type="defaultSemiBold">{`${isOpen ? 'Hide' : 'Show'} ${title}`}</ThemedText>
+        <ThemedText type="defaultSemiBold">{`${
+          isOpen ? "Hide" : "Show"
+        } ${title}`}</ThemedText>
       </TouchableOpacity>
     </ThemedView>
   );
@@ -36,12 +45,13 @@ const styles = StyleSheet.create({
     bottom: 70,
   },
   heading: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 6,
   },
   content: {
     marginBottom: 6,
     marginLeft: 24,
+    borderRadius: 5
   },
 });
