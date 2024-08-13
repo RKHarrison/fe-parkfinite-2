6b786feebb3b9d84c0d7b419b538d80dda6ca204
Richard Harrison <r.harr.k@gmail.com>
2024-08-13 22:45:31 +0100

    adds basic formatting with use of new util function and reusable text component

--- parkfinite2/components/individual-campsite-screen/individual-campsite-card-component.tsx
+++ parkfinite2/components/individual-campsite-screen/individual-campsite-card-component.tsx
@@ -1,12 +1,12 @@
 import { getCampsiteById } from "@/services/api/campsitesApi";
 import { Campsite } from "@/types/api-data-types/campsite-types";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import { ImageCarousel } from "../ImageCarousel";
-import ParallaxScrollView from "../ParallaxScrollView";
 
-import { motorhomeicon } from "@/assets/images/campsite-icons/motorhome-free-icon.png";
 import { convertNumberToStars } from "@/utils/convertNumberToStars";
+import formatDateStamp from "@/utils/formatDateStamp";
+import FieldAndDataText from "@/components/FieldAndDataText";
 
 export default function IndividualCampsiteCard({
   id,
@@ -25,27 +25,40 @@ export default function IndividualCampsiteCard({
     <ScrollView>
       <View style={styles.headerContainer}>
         <Text style={styles.h1}>{loadedCampsite?.campsite_name}</Text>
+        <FieldAndDataText
+          title="Average rating"
+          data={loadedCampsite?.average_rating}
+        />
+        <Text>
+          {loadedCampsite?.average_rating &&
+            convertNumberToStars(loadedCampsite.average_rating)}
+        </Text>
       </View>
       <ImageCarousel campsitePhotos={loadedCampsite?.photos} />
       <View style={styles.container}>
         <Text style={styles.h2}>{loadedCampsite?.category.category_name}</Text>
-        <Text>{convertNumberToStars(loadedCampsite?.average_rating)}</Text>
-        <Text>Average user rating: {loadedCampsite?.average_rating}</Text>
-        <Text>
-          Added on {loadedCampsite?.date_added} by {loadedCampsite?.added_by}
-        </Text>
-        <Text>Description: {loadedCampsite?.description}</Text>
+        <FieldAndDataText
+          title="Added on"
+          data={loadedCampsite && formatDateStamp(loadedCampsite?.date_added)}
+        />
+        <FieldAndDataText title="Added by" data={loadedCampsite?.added_by} />
+        <Text style={{ fontStyle: "italic" }}>{`"${loadedCampsite?.description}"`}</Text>
       </View>
       {loadedCampsite?.contacts[0] && (
         <View style={styles.container}>
           <Text style={styles.h2}>CONTACT INFO</Text>
-          <Text>Name: {loadedCampsite?.contacts[0].campsite_contact_name}</Text>
-          <Text>
-            Phone: {loadedCampsite?.contacts[0].campsite_contact_phone}
-          </Text>
-          <Text>
-            Email: {loadedCampsite?.contacts[0].campsite_contact_email}
-          </Text>
+          <FieldAndDataText
+            title="Name"
+            data={loadedCampsite?.contacts[0].campsite_contact_name}
+          />
+          <FieldAndDataText
+            title="Phone"
+            data={loadedCampsite?.contacts[0].campsite_contact_phone}
+          />
+          <FieldAndDataText
+            title="Email"
+            data={loadedCampsite?.contacts[0].campsite_contact_email}
+          />
         </View>
       )}
     </ScrollView>
@@ -68,6 +81,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    textAlign: "center",
     padding: 10,
     margin: 10,
     alignSelf: "center",
@@ -81,5 +95,5 @@ const styles = StyleSheet.create({
   h2: {
     fontSize: 16,
     fontWeight: "bold",
-  }
+  },
 });
