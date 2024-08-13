0ab45c5a6cdb6110a92435dbec2cbc5f54201ae5
Richard Harrison <r.harr.k@gmail.com>
2024-08-13 13:47:38 +0100

    uses getStars util function to style data visualisation, and props css styling to collapsible component

--- parkfinite2/components/map-screen/campsite-info-card.tsx
+++ parkfinite2/components/map-screen/campsite-info-card.tsx
@@ -1,6 +1,8 @@
 import { Campsite } from "@/types/api-data-types/campsite-types";
 import { Collapsible } from "../Collapsible";
 import { Text } from "react-native";
+import { getStars } from "../GetStars";
+import { StyleSheet, ViewStyle } from "react-native";
 
 export function CampsiteInfoCard({
   selectedCampsite,
@@ -8,11 +10,26 @@ export function CampsiteInfoCard({
   selectedCampsite: Campsite;
 }) {
   return (
-    <Collapsible title={"Campsite Info"}>
-      <Text>{selectedCampsite?.campsite_name}</Text>
-      <Text>Average Rating: {selectedCampsite?.average_rating}</Text>
-      <Text>Date Added: {selectedCampsite?.date_added}</Text>
-      <Text>Description: {selectedCampsite?.description}</Text>
+    <Collapsible title={"Campsite Info"} collapsibleContainerStyle={styles.collapsibleContainer}>
+      <Text style={styles.header}>{selectedCampsite?.campsite_name}</Text>
+      <Text>{getStars(selectedCampsite?.average_rating)}  </Text>
+      <Text>{selectedCampsite?.description}</Text>
     </Collapsible>
   );
 }
+
+const styles = StyleSheet.create({
+    header: {
+        fontWeight: "bold",
+    },
+    collapsibleContainer: {
+        width: "76%",        
+        zIndex: 1000,
+        left: 50,
+        right: 50,
+        bottom: 70,
+        borderRadius: 5,
+        paddingRight: 30,
+        padding: 8,
+    }
+})
