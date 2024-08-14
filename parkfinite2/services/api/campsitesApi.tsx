6c113571b6cbef17602bae253e446d7cbf5e9335
Richard Harrison <r.harr.k@gmail.com>
2024-08-14 12:15:23 +0100

    creates axios request getReviewsByCampsiteId

--- parkfinite2/services/api/campsitesApi.tsx
+++ parkfinite2/services/api/campsitesApi.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Campsite } from "@/types/api-data-types/campsite-types";
+import { Campsite, CampsiteReview } from "@/types/api-data-types/campsite-types";
 
 const parkfinite2Api = axios.create({
   baseURL: "https://parkfinite-api.onrender.com",
@@ -24,6 +24,22 @@ export const getCampsites = () => {
 export function getCampsiteById(campsiteId: string | string[]) {
   return parkfinite2Api
     .get<Campsite>(`/campsites/${campsiteId}`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((error) => {
+      console.error("API Error", {
+        message: error.message,
+        status: error.response?.status,
+        data: error.response?.data,
+      });
+      throw error;
+    });
+}
+
+export function getReviewsByCampsiteId(campsiteId: string | string[]) {
+  return parkfinite2Api
+    .get<CampsiteReview[]>(`/campsites/${campsiteId}/reviews`)
     .then((res) => {
       return res.data;
     })
