637456d275e80a4a516699184f4370c6c91e4aee
Richard Harrison <r.harr.k@gmail.com>
2024-08-14 13:13:25 +0100

    makes api call from CampsiteReviews component on change to propped campsiteId, stores reviews array to state, and renders each review with basic styling

--- parkfinite2/components/individual-campsite-screen/campsite-detailed-components/campsite-reviews.tsx
+++ parkfinite2/components/individual-campsite-screen/campsite-detailed-components/campsite-reviews.tsx
@@ -1,23 +1,35 @@
-import { View } from "react-native";
-import { useEffect } from "react";
+import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
 import { getReviewsByCampsiteId } from "@/services/api/campsitesApi";
+import { CampsiteReview } from "@/types/api-data-types/campsite-types";
+import { campsiteDetailedCardStyles } from "../campsite-detailed-styles";
+import { convertNumberToStars } from "@/utils/convertNumberToStars";
 
 type CampsiteReviewsProps = {
-  campsiteId: string;
+  campsiteId: number;
 };
 
-export default function CampsiteReviews({
-  campsiteId,
-}: CampsiteReviewsProps) {
+export default function CampsiteReviews({ campsiteId }: CampsiteReviewsProps) {
+  const [campsiteReviews, setCampsiteReviews] = useState<CampsiteReview[]>([]);
 
-    useEffect(()=> {
-        getReviewsByCampsiteId(campsiteId).then(reviewsFromApi =>{
-            console.log(reviewsFromApi);
-            
-        })
-    }, [campsiteId])
+  useEffect(() => {
+    getReviewsByCampsiteId(campsiteId).then((reviewsFromApi) => {
+      setCampsiteReviews(reviewsFromApi);
+    });
+  }, [campsiteId]);
 
   return (
-    <View></View>
+    <>
+      <View style={campsiteDetailedCardStyles.container}>
+        <Text style={campsiteDetailedCardStyles.h2}>Reviews</Text>
+        {campsiteReviews.map((review) => (
+          <View style={campsiteDetailedCardStyles.subContainer}>
+            <Text>{convertNumberToStars(review.rating)}</Text>
+            <Text>{review.comment}</Text>
+            <Text style={campsiteDetailedCardStyles.h3}>{review.username}</Text>
+          </View>
+        ))}
+      </View>
+    </>
   );
 }
