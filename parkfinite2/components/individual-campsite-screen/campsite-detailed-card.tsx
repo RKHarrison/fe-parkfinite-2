309f5aca2d0272dabca1fc83a4f5f66fbaee48cc
Richard Harrison <r.harr.k@gmail.com>
2024-08-14 11:27:39 +0100

    refactors conditional rendering of child components

--- parkfinite2/components/individual-campsite-screen/campsite-detailed-card.tsx
+++ parkfinite2/components/individual-campsite-screen/campsite-detailed-card.tsx
@@ -8,6 +8,7 @@ import { convertNumberToStars } from "@/utils/convertNumberToStars";
 import FieldAndDataText from "@/components/FieldAndDataText";
 import CampsiteContacts from "./campsite-detailed-components/campsite-contacts";
 import CampsiteBasicInfo from "./campsite-detailed-components/campsite-basic-info";
+import CampsiteHeaderAndRating from "./campsite-detailed-components/campsite-header-and-rating";
 
 export default function CampsiteDetailedCard({
   id,
@@ -23,56 +24,50 @@ export default function CampsiteDetailedCard({
   }, [id]);
 
   return (
-    <ScrollView>
-      <View style={styles.headerContainer}>
-        <Text style={styles.h1}>{loadedCampsite?.campsite_name}</Text>
-        <FieldAndDataText
-          title="Average rating"
-          data={loadedCampsite?.average_rating}
-        />
-        <Text>
-          {loadedCampsite?.average_rating &&
-            convertNumberToStars(loadedCampsite.average_rating)}
-        </Text>
-      </View>
-      <ImageCarousel campsitePhotos={loadedCampsite?.photos} />
-      {loadedCampsite && <CampsiteBasicInfo campsite={loadedCampsite} />}
-      {loadedCampsite?.contacts[0] && (
-        <CampsiteContacts campsiteContacts={loadedCampsite.contacts} />
+    <>
+      {loadedCampsite && (
+        <ScrollView>
+          <CampsiteHeaderAndRating campsite={loadedCampsite} />
+          <ImageCarousel campsitePhotos={loadedCampsite.photos} />
+          <CampsiteBasicInfo campsite={loadedCampsite} />
+          {loadedCampsite.contacts[0] && (
+            <CampsiteContacts campsiteContacts={loadedCampsite.contacts} />
+          )}
+        </ScrollView>
       )}
-    </ScrollView>
-  );
-}
+    </>
+  )};
 
-const styles = StyleSheet.create({
-  headerContainer: {
-    width: 350,
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 1,
-    marginTop: 10,
-    alignSelf: "center",
-    borderRadius: 10,
-  },
-  container: {
-    width: 350,
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    padding: 10,
-    margin: 10,
-    alignSelf: "center",
-    backgroundColor: "darkseagreen",
-    borderRadius: 10,
-  },
-  h1: {
-    fontSize: 28,
-    fontWeight: "bold",
-  },
-  h2: {
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
+  const styles = StyleSheet.create({
+    headerContainer: {
+      width: 350,
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 1,
+      marginTop: 10,
+      alignSelf: "center",
+      borderRadius: 10,
+    },
+    container: {
+      width: 350,
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
+      padding: 10,
+      margin: 10,
+      alignSelf: "center",
+      backgroundColor: "darkseagreen",
+      borderRadius: 10,
+    },
+    h1: {
+      fontSize: 28,
+      fontWeight: "bold",
+    },
+    h2: {
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+  });
+
