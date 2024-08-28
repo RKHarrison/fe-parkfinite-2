7700117908c5d5f479cd483daadb57cb0fa62262
Richard Harrison <r.harr.k@gmail.com>
2024-08-28 15:48:38 +0100

    adds interceptor to attach bearer token, retrieved from expo-secure-store, to the header of each campsite request in order to allow access to data when user is logged in

--- parkfinite2/services/api/campsitesApi.tsx
+++ parkfinite2/services/api/campsitesApi.tsx
@@ -1,9 +1,24 @@
 import axios from "axios";
+import * as SecureStore from 'expo-secure-store';
 import { Campsite, CampsiteReview } from "@/types/api-data-types/campsite-types";
+import { longPressHandlerName } from "react-native-gesture-handler/lib/typescript/handlers/LongPressGestureHandler";
 
 const parkfinite2Api = axios.create({
   baseURL: "https://parkfinite-2-api.onrender.com/",
 });
+
+parkfinite2Api.interceptors.request.use(
+  async (config) => {
+    const token = await SecureStore.getItemAsync('bearerToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export const getCampsites = () => {
   return parkfinite2Api
