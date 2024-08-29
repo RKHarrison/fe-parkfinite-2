8f80341cebbd11e382a317498bee666e1c1165db
Richard Harrison <r.harr.k@gmail.com>
2024-08-29 11:54:54 +0100

    refactors user context

--- parkfinite2/contexts/UserContext.tsx
+++ parkfinite2/contexts/UserContext.tsx
@@ -1,16 +1,30 @@
 import React, { createContext, useState, ReactNode } from "react";
+import * as SecureStore from "expo-secure-store";
+import { getJsonWebToken } from "@/services/api/authApi";
+import { getUserAccountDataById } from "@/services/api/usersApi";
+import { save } from "@/utils/expoSecureStore";
+import { UserAccountData } from "@/types/api-data-types/user-types";
 
-export const UserContext = createContext<[any, React.Dispatch<any>] | {}>({});
-
+interface LoggedInUser extends UserAccountData {
+  username: string;
+}
 interface UserProviderProps {
   children: ReactNode;
 }
 
+export const UserContext = createContext<{
+  user: LoggedInUser | null;
+  setUser: React.Dispatch<React.SetStateAction<LoggedInUser | null>>;
+}>({
+  user: null,
+  setUser: () => {},
+});
+
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<LoggedInUser | null>(null);
 
   return (
-    <UserContext.Provider value={[user, setUser]}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
