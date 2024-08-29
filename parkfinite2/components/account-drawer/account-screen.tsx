37aab7dfbd6fbbb0c2f110841612bbbbe38a8666
Richard Harrison <r.harr.k@gmail.com>
2024-08-29 13:09:52 +0100

    creates basic account detials card and displayed conditionally on account screen when logged in

--- parkfinite2/components/account-drawer/account-screen.tsx
+++ parkfinite2/components/account-drawer/account-screen.tsx
@@ -3,6 +3,7 @@ import { SafeAreaView } from "react-native";
 import { UserContext } from "@/contexts/UserContext";
 import UserLoginForm from "./account-screen-components/user-login-form";
 import { Button } from "../Button";
+import UserAccountCard from "./account-screen-components/user-account-card";
 
 export default function AccountScreen() {
   const {user, logout} = useContext(UserContext)
@@ -10,6 +11,7 @@ export default function AccountScreen() {
   return (
     <SafeAreaView>
       {!user && <UserLoginForm/>}
+      {user && <UserAccountCard/>}
       {user && <Button title={"Log out"} onPress={logout}/>}
     </SafeAreaView>
   );
