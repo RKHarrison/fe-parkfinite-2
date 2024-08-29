d1332018321801a601f382cfabfb39cf005e7fd3
Richard Harrison <r.harr.k@gmail.com>
2024-08-29 17:22:47 +0100

    adds background colur in drawer view and removes scroll behaviour

--- parkfinite2/components/navigation/CustomDrawerContent.tsx
+++ parkfinite2/components/navigation/CustomDrawerContent.tsx
@@ -2,17 +2,24 @@ import { UserContext } from "@/contexts/UserContext";
 import {
   DrawerContentScrollView,
   DrawerItemList,
-  DrawerItem
+  DrawerItem,
 } from "@react-navigation/drawer";
 import { useContext } from "react";
+import { View } from "react-native";
 
 export default function CustomDrawerContent(props: any) {
-  const {user, logout} = useContext(UserContext)
+  const { user, logout } = useContext(UserContext);
 
   return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props}/>
-      {user && <DrawerItem label={"logout"} onPress={() => logout()}/>}
-    </DrawerContentScrollView>
+    <View style={{ flex: 1 }}>
+      <DrawerContentScrollView
+        {...props}
+        scrollEnabled={false}
+        contentContainerStyle={{backgroundColor: "#ebf9eb"}}
+      >
+        <DrawerItemList {...props}/>
+        {user && <DrawerItem label={"logout"} onPress={() => logout()} />}
+      </DrawerContentScrollView>
+    </View>
   );
 }
