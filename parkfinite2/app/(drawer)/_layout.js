88f138baa6c7e87324070e751a4ea34be54644f3
Richard Harrison <r.harr.k@gmail.com>
2024-08-29 17:23:49 +0100

    adds status bar hide animation and selected background color

--- parkfinite2/app/(drawer)/_layout.js
+++ parkfinite2/app/(drawer)/_layout.js
@@ -2,40 +2,54 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
 import { UserContext } from "@/contexts/UserContext";
 import { useContext } from "react";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import CustomDrawerContent from "../../components/navigation/CustomDrawerContent"
+import Ionicons from "@expo/vector-icons/Ionicons";
+import CustomDrawerContent from "../../components/navigation/CustomDrawerContent";
+import { DrawerStatusContext } from "@react-navigation/drawer";
 
 export default function Layout() {
   const { user } = useContext(UserContext);
 
   return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Drawer drawerContent={CustomDrawerContent}>
-          <Drawer.Screen
-            name="(tabs)"
-            options={{
-              drawerLabel: "Go to Map screen...",
-              title: "Map",
-              drawerIcon: ({size, color}) => (<Ionicons name="map-outline" size={size} color={color}/>)
-            }}
-          />
-          <Drawer.Screen
-            name="account"
-            options={{
-              drawerLabel: user ? "My Account" : "Login",
-              title: user ? "Account Details" : "Login Page",
-              drawerIcon: ({size, color}) => (<Ionicons name="person-outline" size={size} color={color}/>)
-            }}
-          />
-          <Drawer.Screen
-            name="settings"
-            options={{
-              drawerLabel: "Settings",
-              title: "Settings",
-              drawerIcon: ({size, color}) => (<Ionicons name="settings-outline" size={size} color={color}/>)
-            }}
-          />
-        </Drawer>
-      </GestureHandlerRootView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        drawerContent={CustomDrawerContent}
+        screenOptions={{
+          drawerHideStatusBarOnOpen: true,
+          drawerStatusBarAnimation: "fade",
+          drawerActiveTintColor: "#2a892a",
+        }}
+      >
+        <Drawer.Screen
+          name="(tabs)"
+          options={{
+            drawerLabel: "Go to Map screen...",
+            title: "Map",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name={ "map-outline"} size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="account"
+          options={{
+            drawerLabel: user ? "My Account" : "Login",
+            title: user ? "Account Details" : "Login Page",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="person-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="settings"
+          options={{
+            drawerLabel: "Settings",
+            title: "Settings",
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="settings-outline" size={size} color={color} />
+            ),
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
