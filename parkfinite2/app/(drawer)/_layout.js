64a150e3ad18b34b19741a495bdc8978467fd327
Richard Harrison <r.harr.k@gmail.com>
2024-08-30 10:44:18 +0100

    assigns rendering of navigation header to custom tsx component

--- parkfinite2/app/(drawer)/_layout.js
+++ parkfinite2/app/(drawer)/_layout.js
@@ -4,7 +4,7 @@ import { UserContext } from "@/contexts/UserContext";
 import { useContext } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import CustomDrawerContent from "../../components/navigation/CustomDrawerContent";
-import { DrawerStatusContext } from "@react-navigation/drawer";
+import CustomDrawerNavigationHeader from "../../components/navigation/CustomDrawerNavigationHeader"
 
 export default function Layout() {
   const { user } = useContext(UserContext);
@@ -13,12 +13,13 @@ export default function Layout() {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer
         drawerContent={CustomDrawerContent}
-        screenOptions={{
+        screenOptions={({ navigation }) => ({
+          header: () => <CustomDrawerNavigationHeader navigation={navigation} />,
           drawerHideStatusBarOnOpen: true,
           drawerStatusBarAnimation: "fade",
           drawerActiveTintColor: "#2a892a",
-          drawerLabelStyle: {marginLeft: -20}
-        }}
+          drawerLabelStyle: { marginLeft: -20 },
+        })}
       >
         <Drawer.Screen
           name="(tabs)"
