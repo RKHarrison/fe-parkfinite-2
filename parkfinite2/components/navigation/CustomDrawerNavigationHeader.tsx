79baaebd706806044b0c0f09937f74530a0d1dc4
Richard Harrison <r.harr.k@gmail.com>
2024-08-30 11:08:54 +0100

    Adds condtitional user icon to replace default menu icon when logged in

--- parkfinite2/components/navigation/CustomDrawerNavigationHeader.tsx
+++ parkfinite2/components/navigation/CustomDrawerNavigationHeader.tsx
@@ -1,41 +1,61 @@
 import { Ionicons } from "@expo/vector-icons";
-import { StyleSheet, View, TextInput } from "react-native";
-import { DrawerNavigationProp } from '@react-navigation/drawer';
-
+import { StyleSheet, View, TextInput, Image } from "react-native";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { useContext } from "react";
+import { UserContext } from "@/contexts/UserContext";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 type CustomDrawerNavigationHeaderProps = {
-    navigation: DrawerNavigationProp<any>
+  navigation: DrawerNavigationProp<any>;
+};
+
+export default function CustomDrawerNavigationHeader({
+  navigation,
+}: CustomDrawerNavigationHeaderProps) {
+  const { user } = useContext(UserContext);
+
+  return (
+    <View style={styles.headerContainer}>
+      {user ? (
+        <TouchableOpacity onPress={() => navigation.openDrawer()}>
+          <Image
+            source={require("@/assets/images/dummy_profile_pic.jpeg")}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+      ) : (
+        <Ionicons
+          name="menu"
+          size={28}
+          onPress={() => navigation.openDrawer()}
+        />
+      )}
+    </View>
+  );
 }
 
-export default function CustomDrawerNavigationHeader({navigation}:CustomDrawerNavigationHeaderProps) {
-
-    return (
-        <View style={styles.headerContainer}>
-          <Ionicons
-            name="menu"
-            size={28}
-            onPress={() => navigation.openDrawer()}
-          />
-        </View>
-      );
-    };
-    
-    const styles = StyleSheet.create({
-      headerContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: 10,
-        height: 60,
-        paddingVertical: 5,
-        backgroundColor: '#d8f3d8',
-      },
-      searchInput: {
-        flex: 1,
-        height: 40,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        paddingHorizontal: 10,
-        marginLeft: 10,
-      },
-    });
-    
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    height: 65,
+    paddingVertical: 5,
+    paddingTop: 25,
+    backgroundColor: "#afcfaf",
+  },
+  searchInput: {
+    flex: 1,
+    height: 40,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginLeft: 10,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    marginHorizontal: 2,
+    borderRadius: 50,
+  },
+});
