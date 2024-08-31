4d76f51383d5719528dd60df108199750a331ec5
Richard Harrison <r.harr.k@gmail.com>
2024-08-31 10:09:45 +0100

    places form in a scroll view to fit content

--- parkfinite2/components/map-drawer/post-new-campsite-tab/post-new-campsite-screen.tsx
+++ parkfinite2/components/map-drawer/post-new-campsite-tab/post-new-campsite-screen.tsx
@@ -1,11 +1,12 @@
 import { View, Text, StyleSheet } from "react-native";
 import PostNewCampsiteForm from "./post-new-campsite-components/post-new-campsite-form";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function PostNewCampsiteScreen() {
   return (
-    <View>
+    <ScrollView>
       <PostNewCampsiteForm />
-    </View>
+    </ScrollView>
   );
 }
 
