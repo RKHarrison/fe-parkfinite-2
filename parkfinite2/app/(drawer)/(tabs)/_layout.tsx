fa224e3ccad5dc2853e359cb7bc8706f903f4936
Richard Harrison <r.harr.k@gmail.com>
2024-08-31 16:13:41 +0100

    wraps tabs in CustomCoordinatesContext Provider

--- parkfinite2/app/(drawer)/(tabs)/_layout.tsx
+++ parkfinite2/app/(drawer)/(tabs)/_layout.tsx
@@ -5,42 +5,51 @@ import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "react-native";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { UserContext } from "@/contexts/UserContext";
+import { CustomCoordinatesProvider } from "@/contexts/CustomCoordinatesContext";
 
 export default function TabLayout() {
   const { user } = useContext(UserContext);
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: true,
-        headerStyle: {
-          backgroundColor: "green",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="search"
-        options={{
-          headerShown: false,
-          title: "Go to Map",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? "map" : "map-outline"} color={color} />
-          ),
+    <CustomCoordinatesProvider>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "green",
+          },
         }}
-      />
-      <Tabs.Screen
-        name="post-new-campsite"
-        options={{
-          headerShown: false,
-          title: "Post new campsite",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? "pin" : "pin-outline"} color={color} />
-          ),
-          href: "/(drawer)/(tabs)/post-new-campsite",
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="search"
+          options={{
+            headerShown: false,
+            title: "Go to Map",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon
+                name={focused ? "map" : "map-outline"}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="post-new-campsite"
+          options={{
+            headerShown: false,
+            title: "Post new campsite",
+            tabBarIcon: ({ color, focused }) => (
+              <TabBarIcon
+                name={focused ? "pin" : "pin-outline"}
+                color={color}
+              />
+            ),
+            href: "/(drawer)/(tabs)/post-new-campsite",
+          }}
+        />
+      </Tabs>
+    </CustomCoordinatesProvider>
   );
 }
