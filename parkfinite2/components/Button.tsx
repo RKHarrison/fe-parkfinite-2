e6b63a8bb260033da68f02450e469d82334b1ff7
Richard Harrison <r.harr.k@gmail.com>
2024-09-02 18:25:14 +0100

    removes padding from reusable button component

--- parkfinite2/components/Button.tsx
+++ parkfinite2/components/Button.tsx
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: "green",
     borderRadius: 12,
-    width: "80%",
+    width: "100%",
     height: 30,
     minWidth: 80,
     alignItems: 'center',
