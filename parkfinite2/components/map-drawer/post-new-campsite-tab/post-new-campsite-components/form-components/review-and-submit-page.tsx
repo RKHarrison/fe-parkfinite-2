af313a445e91c6630ea57d4c198d4d442868c4b2
Richard Harrison <r.harr.k@gmail.com>
2024-09-06 12:42:16 +0100

    adds button to confirm changes and make an http post request to api

--- parkfinite2/components/map-drawer/post-new-campsite-tab/post-new-campsite-components/form-components/review-and-submit-page.tsx
+++ parkfinite2/components/map-drawer/post-new-campsite-tab/post-new-campsite-components/form-components/review-and-submit-page.tsx
@@ -1,8 +1,11 @@
-import { Text, View, ScrollView } from "react-native";
+import { Text, View, ScrollView, StyleSheet } from "react-native";
 import { Button } from "@/components/Button";
 import { CampsitePostRequest } from "@/types/api-data-types/campsite-types";
 import FieldAndDataText from "@/components/FieldAndDataText";
 import { campsiteCategoryMap } from "./new-campsite-basic-info-form";
+import { postCampsite } from "@/services/api/campsitesApi";
+import { useContext } from "react";
+import { UserContext } from "@/contexts/UserContext";
 
 interface ReviewAndSubmitNewCampsiteProps {
   newCampsiteAddress: string;
@@ -15,15 +18,28 @@ export default function ReviewAndSubmitNewCampsite({
   newCampsiteData,
   setFormStep,
 }: ReviewAndSubmitNewCampsiteProps) {
+  const { user, logout } = useContext(UserContext);
+
+  function handleSubmitNewCampsite() {
+    if (user) {
+      const fullCampsiteData = {
+        ...newCampsiteData,
+        user_account_id: user.user_account_id,
+      };
+      postCampsite(fullCampsiteData, logout);
+    }
+  }
+
   return (
     <>
-      <Text>Location: {newCampsiteAddress}</Text>
+      <Text style={styles.h1}>Location</Text>
+      <Text>{newCampsiteAddress}</Text>
       <Button
         title="Go back to choose location"
         onPress={() => setFormStep(1)}
       />
 
-      <Text>Basic Info: </Text>
+      <Text style={styles.h1}>Basic Info</Text>
       <FieldAndDataText title="Name" data={newCampsiteData.campsite_name} />
       <FieldAndDataText
         title="Category"
@@ -51,11 +67,17 @@ export default function ReviewAndSubmitNewCampsite({
       />
       <Button title="Go back to basic info" onPress={() => setFormStep(2)} />
 
-      <Text>Contact(s): </Text>
+      <Text style={styles.h1}>Contact(s)</Text>
       {newCampsiteData.contacts ? (
-        newCampsiteData.contacts.map((contact, index) => (
-          <ScrollView>
-            <View key={index} style={{ margin: 10 }}>
+        <ScrollView
+          style={
+            newCampsiteData.contacts.length > 1
+              ? styles.scrollViewMultiple
+              : styles.scrollViewSingle
+          }
+        >
+          {newCampsiteData.contacts.map((contact, index) => (
+            <View key={index} style={styles.contactContainer}>
               <FieldAndDataText
                 title="Name"
                 data={contact.campsite_contact_name}
@@ -71,12 +93,30 @@ export default function ReviewAndSubmitNewCampsite({
                 />
               )}
             </View>
-          </ScrollView>
-        ))
+          ))}
+        </ScrollView>
       ) : (
         <Text>No contacts provided for this spot.</Text>
       )}
       <Button title="Go back to contacts" onPress={() => setFormStep(3)} />
+
+      <Button
+        title="Confirm campsite details and submit new campsite!"
+        onPress={() => handleSubmitNewCampsite()}
+      />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  h1: { fontSize: 18, fontWeight: "bold", marginTop: 20, marginBottom: 3 },
+  scrollViewMultiple: {
+    height: 200,
+  },
+  scrollViewSingle: {
+    maxHeight: 65,
+  },
+  contactContainer: {
+    margin: 10,
+  },
+});
