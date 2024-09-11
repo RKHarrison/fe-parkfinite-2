b12b52e85de0b991e15f64535e4dd2b5170b6a4c
Richard Harrison <r.harr.k@gmail.com>
2024-09-11 17:07:13 +0100

    adds interceptor to handle expired bearer token, handling case where expired user attemtps to post new campsite elegantly

--- parkfinite2/services/api/campsitesApi.tsx
+++ parkfinite2/services/api/campsitesApi.tsx
@@ -1,45 +1,58 @@
 import axios from "axios";
-import * as SecureStore from 'expo-secure-store';
-import { Campsite, CampsitePostRequest, CampsiteReview, CampsiteReviewPostRequest } from "@/types/api-data-types/campsite-types";
+import * as SecureStore from "expo-secure-store";
+import {
+  Campsite,
+  CampsitePostRequest,
+  CampsiteReview,
+  CampsiteReviewPostRequest,
+} from "@/types/api-data-types/campsite-types";
 import { router } from "expo-router";
-import { UserContext } from "@/contexts/UserContext";
-import { useContext } from "react";
 
 const parkfinite2Api = axios.create({
   baseURL: "https://parkfinite-2-api.onrender.com/",
 });
 
-
 parkfinite2Api.interceptors.request.use(
   async (config) => {
-    const token = await SecureStore.getItemAsync('bearerToken');
+    const token = await SecureStore.getItemAsync("bearerToken");
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
   },
   (error) => {
-    throw error;
+    return Promise.reject(error);
+  }
+);
+
+parkfinite2Api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      alert("Please log in again, session has expired.");
+      router.push("/(drawer)/account");
+    }
+    return Promise.reject(error);
   }
 );
 
 export const postCampsite = (newCampsiteData: CampsitePostRequest) => {
   return parkfinite2Api
     .post("/campsites", newCampsiteData)
-    .then(response => response.data)
+    .then((response) => response.data)
     .catch((error) => {
       console.error("API Error", {
         message: error.message,
         status: error.response?.status,
-        data: error.response?.data.detail
+        data: error.response?.data.detail,
       });
       if (error.status === 401) {
-        alert('Please log in again, session has expired.')
-        router.push('/(drawer)/account')
+        alert("Please log in again, session has expired.");
+        router.push("/(drawer)/account");
       }
       throw error;
     });
-}
+};
 
 export const getCampsites = () => {
   return parkfinite2Api
@@ -73,7 +86,10 @@ export function getCampsiteById(campsiteId: string | string[]) {
     });
 }
 
-export function postReviewByCampsiteId(campsiteId: number, review: CampsiteReviewPostRequest) {
+export function postReviewByCampsiteId(
+  campsiteId: number,
+  review: CampsiteReviewPostRequest
+) {
   return parkfinite2Api
     .post<CampsiteReview>(`/campsites/${campsiteId}/reviews`, review)
     .then((res) => {
