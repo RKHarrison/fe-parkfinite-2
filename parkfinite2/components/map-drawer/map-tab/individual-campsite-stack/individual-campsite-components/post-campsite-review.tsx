8ca5068d4b03fd3ee0807c1b9a2ad4655ecb2801
Richard Harrison <r.harr.k@gmail.com>
2024-09-12 17:06:45 +0100

    adds StarRating component for use in review process

--- parkfinite2/components/map-drawer/map-tab/individual-campsite-stack/individual-campsite-components/post-campsite-review.tsx
+++ parkfinite2/components/map-drawer/map-tab/individual-campsite-stack/individual-campsite-components/post-campsite-review.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
 import { campsiteDetailedCardStyles } from "../individual-campsite-styles";
+import StarRatingComponent, { StarRating } from "@/components/StarRating";
 
 type PostCampsiteReviewProps = {
-    campsiteId: number;
-}
+  campsiteId: number;
+};
 
-export default function PostCampsiteReview({campsiteId} : PostCampsiteReviewProps) {
+export default function PostCampsiteReview({
+  campsiteId,
+}: PostCampsiteReviewProps) {
+  const [rating, setRating] = useState<StarRating>(null);
+
+  const handleRatingChange = (rating: number) => {
+    setRating(rating);
+  };
+
   return (
     <View style={campsiteDetailedCardStyles.container}>
       <Text>PostCampsiteReview</Text>
+      <StarRatingComponent
+        initialRating={rating}
+        onRatingChange={handleRatingChange}
+      />
     </View>
   );
-} 
+}
